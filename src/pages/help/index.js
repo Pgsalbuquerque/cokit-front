778c8006033dd6b735a006d176b0af6872266714
Vitória Bezerra <56508704+vitoriabezerra@users.noreply.github.com>
2021-12-03 17:44:07 -0300

    chatbot da pagina help

--- src/pages/help/index.js
+++ src/pages/help/index.js
@@ -1,10 +1,35 @@
 import React from "react";
 
+import { useState } from "react";
+
 import { Text, Flex, Box, Input, Button, Icon } from "@chakra-ui/react";
 import Fundo from "../../assets/fundo.png";
 import { BiChevronRight } from "react-icons/bi";
+import Messages from "./messages";
 
 export const Ajuda = () => {
+  const [inputData, setInputData] = useState("");
+
+  const [messages, setMessages] = useState([]);
+
+  const handleAddMessageClick = () => {
+    handleTaskAddition(inputData);
+    setInputData("");
+  };
+
+  const handleTaskAddition = (texto) => {
+    const newMessages = [
+      ...messages,
+      {
+        conteudo: texto,
+      },
+    ];
+    setMessages(newMessages);
+  };
+  const handleInputChange = (e) => {
+    setInputData(e.target.value);
+  };
+
   return (
     <Flex
       backgroundImage={Fundo}
@@ -32,20 +57,30 @@ export const Ajuda = () => {
             alignItems="center"
           >
             <Text as="h2" fontSize="16px" color="#505565" textAlign="center">
-              A equipe da Cokit nunca pedirá senha ou qualquer informação sigilosa.
+              A equipe da Cokit nunca pedirá senha ou qualquer informação
+              sigilosa.
             </Text>
-            <Flex h="70%" flexDirection="column">
-                <Flex mt="10px" h="2rem" backgroundColor="#DCF8C6" borderRadius="6px" padding=" 5px 10px 5px 10px" alignItems="center">
-                    <Text as ="small" fontSize="14px" fontWeight="bold" color="#505565">
-                        Preciso de ajuda!
-                    </Text>
-                </Flex>
-                <Flex  mt="10px" h="2rem" backgroundColor="#9EF7F3" borderRadius="6px" padding=" 5px 10px 5px 10px" alignItems="center">
-                    <Text as ="small" fontSize="14px" fontWeight="bold" color="#505565">
-                        Conte-nos seu problema
-                    </Text>
-                </Flex>
-                
+            <Flex h="70%" w="100%" flexDirection="column" alignItems="flex-end" mr="20px">
+              {/* <Flex
+                mt="10px"
+                h="2rem"
+                backgroundColor="#DCF8C6"
+                borderRadius="6px"
+                padding=" 5px 10px 5px 10px"
+                alignItems="center"
+              >
+                <Text
+                  as="small"
+                  fontSize="14px"
+                  fontWeight="bold"
+                  color="#505565"
+                >
+                  Preciso de ajuda!
+                </Text>
+              </Flex> */}
+              <Messages
+                    messages={messages}
+                />
             </Flex>
             <Flex
               marginTop="20px"
@@ -53,25 +88,30 @@ export const Ajuda = () => {
               justifyContent="center"
             >
               <Input
+                ml="8px"
                 type="name"
                 h="40px"
                 textIndent="10"
-                w="16rem"
+                w="15rem"
                 color="#525050"
                 fontWeight="600"
                 border="none"
                 backgroundColor="#322F38"
                 borderRadius="6px"
                 color="white"
+                value={inputData}
+                onChange={handleInputChange}
               />
-              <Icon
-                as={BiChevronRight}
-                fontSize="42px"
-                color="#313131"
-                backgroundColor="#FFC633"
-                borderRadius="3px"
-                ml="4px"
-              />
+              <Button onClick={handleAddMessageClick} border="none">
+                <Icon
+                  as={BiChevronRight}
+                  fontSize="42px"
+                  color="#313131"
+                  backgroundColor="#FFC633"
+                  borderRadius="3px"
+                  ml="2px"
+                />
+              </Button>
             </Flex>
           </Flex>
         </Flex>
